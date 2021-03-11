17638a512e28f7e69b11644fa1b9adf238b38fa6
Maël Naccache Tüfekçi <contact@maeln.com>
2021-03-11 14:09:17 +0100

    update doc on the bitonic example.

--- examples/sorting/main.rs
+++ examples/sorting/main.rs
@@ -5,24 +5,23 @@ use std::rc::Rc;
 use wyzoid::utils::to_csv;
 use wyzoid::{high, utils};
 
+/**
+ * Implement a simple local bitonic merge sort. 
+*/
 fn main() {
     let DATA_LEN = 32;
-    // We generate 64 random float between 0.0 and 1.0.
     let input: Vec<f32> = utils::rand_vec::<f32>(DATA_LEN, 0.0, 128.0);
     let output: Vec<f32> = vec![0.0; DATA_LEN];
 
-    // We use a simple shader that multiply our input by two.
     let shader = PathBuf::from("examples/shaders/bin/examples/bitonic1.cs.spirv");
 
     let vulkan = Rc::new(wyzoid::low::vkstate::init_vulkan());
 
-    // We create the compute job.
-    // Since our shader has a local work size of 64, we divide the number of data by 64 for the dispatch.
     let mut job = high::job::JobBuilder::new()
         .add_buffer(&input, 0, 0)
         .add_buffer(&output, 0, 1)
         .add_shader(&shader)
-        .add_dispatch((32, 1, 1))
+        .add_dispatch((DATA_LEN, 1, 1))
         .build(vulkan);
 
     job.execute();
